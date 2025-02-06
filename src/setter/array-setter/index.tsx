04b4ac02b5c9ef06a57541158300adcf94e3b781
mazhaobo <mazhaobo@hoteamsoft.com>
2025-02-06 09:52:40 +0800

    refactor(setter): 优化数组设定器的值设置逻辑
    - 移除了不必要的 item.setValue(value[i]) 调用
    - 简化了代码结构，提高了执行效率

--- src/setter/array-setter/index.tsx
+++ src/setter/array-setter/index.tsx
@@ -100,7 +100,6 @@ export class ListSetter extends Component<ArraySetterProps, ArraySetterState> {
             },
           },
         });
-        item.setValue(value[i]);
       }
       items.push(item);
     }
